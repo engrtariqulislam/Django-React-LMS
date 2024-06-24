a9a80adf80b5041d6a4f54ae91a47cd31c946af8
Tariqul islam <tnawayn83@yahoo.com>
2024-06-25 00:29:02 +0300

    2.Django Project Setup
    Django Project Setup

--- Django React LMS/backend/lms_backend/settings.py
+++ Django React LMS/backend/lms_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'lms_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [Os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[os.path.join(BASE_DIR, "static")]
+
+STATIC_ROOT = BASE_DIR / 'templates'
+
+MEDIA_URL = '/MEDIA/' #127.0.0.1/media
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
